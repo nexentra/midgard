2677b29a83f048fc967ac3c1d2caf04314ddc24c
towhid <ktowhid20@gmail.com>
2023-08-25 00:49:41 +0600

    added bash script for running frontend backend parallelly

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    run_frontend=true
+    run_backend=true
+else
+    if [ "$1" == "frontend" ]; then
+        run_frontend=true
+        run_backend=false
+    elif [ "$1" == "backend" ]; then
+        run_frontend=false
+        run_backend=true
+    else
+        echo "Invalid argument. Usage: $0 [frontend|backend]"
+        exit 1
+    fi
+fi
+
+tmux new-session -d bash
+tmux split-window -h bash
+
+if [ "$run_frontend" = true ]; then
+    tmux send -t 0:0.0 "cd ./api/ui && yarn start" C-m
+fi
+
+if [ "$run_backend" = true ]; then
+    tmux send -t 0:0.1 "GOFLAGS=-mod=mod go run ./" C-m
+fi
+
+tmux -2 attach-session -d
