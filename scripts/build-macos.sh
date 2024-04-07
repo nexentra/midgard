21bab85b0a9d5e14db64e23f204cce73fa2de62a
KnockOutEZ <ktowhid20@gmail.com>
2024-04-07 23:34:05 +0600

    fixed build-macos.sh for dmg and pkg builds

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -4,6 +4,9 @@ echo -e "Start running the script..."
 cd ../
 
 echo -e "Start building the app for macos platform..."
+sudo rm -rf build/bin/*
 wails build --clean --platform darwin/universal
+sudo productbuild --component ./build/bin/midgard.app / ./build/bin/midgard.pkg
+hdiutil create -volname "midgard" -srcfolder "./build/bin/midgard.app" -ov -format UDZO "./build/bin/midgard.dmg"
 
 echo -e "End running the script!"
